f5e67afe9c046c5f891fa7db69f9a84f6e03aac7
Ömer YİĞİTLER <omeryigitler@hotmail.com>
2026-01-09 22:32:18 +0100

    Fix: Add cache busting to status polling

--- netlify/functions/check_status.js
+++ netlify/functions/check_status.js
@@ -36,6 +36,11 @@ exports.handler = async (event, context) => {
 
         return {
             statusCode: 200,
+            headers: {
+                'Cache-Control': 'no-cache, no-store, must-revalidate',
+                'Pragma': 'no-cache',
+                'Expires': '0'
+            },
             body: JSON.stringify({
                 status: doc.data().status
             })
